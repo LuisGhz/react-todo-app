c063af2da913d1fd98f7a88821bf600348ce7c9d
Luis Antonio Gutiérrez Hernández <luisghtz@outlook.com>
2022-02-16 15:08:28 -0600

    Integrate TodoHeader

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,9 +1,12 @@
 import { AddTodoButton } from "components/AddTodoButton";
+import { TodoHeader } from "components/TodoHeader";
 
 export const Home = () => {
   return (
     <section className="todo">
-      <section className="todo__header"></section>
+      <section className="todo__header">
+        <TodoHeader />
+      </section>
       <section className="todo__todo-list"></section>
       <AddTodoButton />
     </section>
