23fbe97130d7d97feeabe61d9c1ea44de7aa8d63
Luis Antonio Gutiérrez Hernández <luisghtz@outlook.com>
2022-02-16 17:03:43 -0600

    Add styles

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,14 +1,15 @@
 import { AddTodoButton } from "components/AddTodoButton";
 import { TodoHeader } from "components/TodoHeader";
 import { TodoList } from "components/TodoList";
+import './Home.css'
 
 export const Home = () => {
   return (
-    <section className="todo">
-      <section className="todo__header">
+    <section className="todo-app">
+      <section className="todo-app__header">
         <TodoHeader />
       </section>
-      <section className="todo__todo-list">
+      <section className="todo-app__todo-list">
         <TodoList />
       </section>
       <AddTodoButton />
