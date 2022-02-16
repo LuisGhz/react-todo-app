0e8c4aaf628252fd8dc7ab48c304d4d1f62d55d1
Luis Antonio Gutiérrez Hernández <luisghtz@outlook.com>
2022-02-16 17:03:36 -0600

    Add styles

--- src/components/TodoHeader.jsx
+++ src/components/TodoHeader.jsx
@@ -1,16 +1,21 @@
-import React from 'react';
-import { format } from 'date-fns';
+import React from "react";
+import { format } from "date-fns";
+import './TodoHeader.css'
 
 export const TodoHeader = ({ totalTasks = 0, completedTasks = 0 }) => {
   return (
     <React.Fragment>
-      <h1>{format(new Date(), 'dd/MM/yyyy')}</h1>
-      <section className="todo__created-tasks">
-        {totalTasks} { totalTasks === 1 ? 'task' : 'tasks' } created
-      </section>
-      <section className="todo__completed-tasks">
-        {completedTasks} { completedTasks === 1 ? 'task' : 'tasks' } completed
+      <h1 className='todo-header todo-app__todo-header'>
+        {format(new Date(), 'dd/MM/yyyy')}
+      </h1>
+      <section className='todo-header__counter'>
+        <section className='todo-header__created-tasks'>
+          <span className='text-bold' >{totalTasks}</span> {totalTasks === 1 ? 'task' : 'tasks'} created
+        </section>
+        <section className='todo-header__completed-tasks'>
+          <span className='text-bold' >{completedTasks}</span> {completedTasks === 1 ? 'task' : 'tasks'} completed
+        </section>
       </section>
     </React.Fragment>
   );
-}
+};
