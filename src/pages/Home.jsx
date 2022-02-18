a9ccf3a3838774f1e83194f6b8b841dc69fddc18
Luis Antonio Gutiérrez Hernández <luisghtz@outlook.com>
2022-02-18 11:42:02 -0600

    Add AddTodo component

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,5 +1,6 @@
 import { AddTodoButton } from "components/AddTodoButton";
 import { TodoHeader } from "components/TodoHeader";
+import { AddTodo } from "components/AddTodo";
 import { TodoProvider } from "TodoContext";
 import { TodoList } from "components/TodoList";
 import "./Home.css";
@@ -7,6 +8,9 @@ import "./Home.css";
 export const Home = () => {
   return (
     <TodoProvider>
+      <div className="todo-app__add-todo-container">
+        <AddTodo />
+      </div>
       <section className="todo-app">
         <section className="todo-app__header">
           <TodoHeader />
