73583b2194790aabd7a5ea6fffd31e180aa6d11d
Luis Antonio Gutiérrez Hernández <luisghtz@outlook.com>
2022-02-17 19:28:35 -0600

    Fix tasks state

--- src/TodoContext.jsx
+++ src/TodoContext.jsx
@@ -3,7 +3,7 @@ import React, { useState, useEffect } from 'react';
 export const TodoContext = React.createContext()
 
 export const TodoProvider = ({ children }) => {
-  const [tasks, setTasks] = useState(0);
+  const [tasks, setTasks] = useState([]);
   const [totalTasksCompleted, setTotalTasksCompleted] = useState(0);
 
   useEffect(() => {
