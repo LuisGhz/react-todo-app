314892074a42aa0d154457ceadedff6873bab85f
Luis Antonio Gutiérrez Hernández <luisghtz@outlook.com>
2022-02-17 19:21:03 -0600

    Changes totalTasks to tasks. Add use effect to update total tasks completed

--- src/TodoContext.jsx
+++ src/TodoContext.jsx
@@ -1,15 +1,20 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 
 export const TodoContext = React.createContext()
 
 export const TodoProvider = ({ children }) => {
-  const [totalTasks, setTotalTasks] = useState(0);
+  const [tasks, setTasks] = useState(0);
   const [totalTasksCompleted, setTotalTasksCompleted] = useState(0);
+
+  useEffect(() => {
+    const totalT = tasks.filter(t => t.isCompleted).length;
+    setTotalTasksCompleted(totalT);
+  }, [tasks]);
 
   return (
     <TodoContext.Provider value={{
-      totalTasks,
-      setTotalTasks,
+      tasks,
+      setTasks,
       totalTasksCompleted,
       setTotalTasksCompleted
     }}>
