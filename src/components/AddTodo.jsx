b18c0480b410cea84fd533d5caedca8ffd7bd941
Luis Antonio Gutiérrez Hernández <luisghtz@outlook.com>
2022-02-21 19:36:14 -0600

    Add axios

--- src/components/AddTodo.jsx
+++ src/components/AddTodo.jsx
@@ -1,12 +1,25 @@
 import { useContext, useState } from "react"
 import { TodoContext } from "TodoContext"
+import axios from 'axios';
 
-export const AddTodo = ({ cancelAddTodo }) => {
+export const AddTodo = ({ setIsAddTodoVisible }) => {
   const [todoDescription, setTodoDescription] = useState('');
-  const { setTasks } = useContext(TodoContext);
+  const { setTasks, tasks = [] } = useContext(TodoContext);
 
   const onCancel = () => {
-    cancelAddTodo(false);
+    setIsAddTodoVisible(false);
+  }
+
+  const addTodo = () => {
+    const newTask = {
+      description: todoDescription,
+      createdAt: new Date(),
+      isCompleted: false
+    };
+    axios.post('http://localhost:3000/todos', newTask);
+    const newTasks = [...tasks, newTask];
+    setTasks(newTasks);
+    setIsAddTodoVisible(false);
   }
 
   return (
@@ -14,7 +27,7 @@ export const AddTodo = ({ cancelAddTodo }) => {
       <h1>Add TODO</h1>
       <input type="text" className="todo-description" onChange={e => setTodoDescription(e.target.value)} />
       <button onClick={onCancel} className="add-todo__cancel" >Cancel</button>
-      <button className="add-todo__add" disabled={todoDescription.trim() === '' ? true : false} >Add</button>
+      <button className="add-todo__add" disabled={todoDescription.trim() === '' ? true : false} onClick={addTodo} >Add</button>
     </>
-  )
+  );
 }
