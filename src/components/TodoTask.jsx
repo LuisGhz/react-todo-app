dbbd4425b86d6d33fbcbf80d9a2dd06551400a43
Luis Antonio Gutiérrez Hernández <luisghtz@outlook.com>
2022-02-23 16:58:18 -0600

    Add styles

--- src/components/TodoTask.jsx
+++ src/components/TodoTask.jsx
@@ -1,10 +1,10 @@
 import { format } from "date-fns";
 import { es } from "date-fns/locale";
+import "./TodoTask.css";
 
 export const TodoTask = ({ task, markAsCompleted, removeTask }) => {
-  
   return (
-    <article className="todo-task">
+    <article className="todo-task" >
       <span
         className={`todo-task__checkmark ${
           task.isCompleted
@@ -15,9 +15,9 @@ export const TodoTask = ({ task, markAsCompleted, removeTask }) => {
       >
         &#x2713;
       </span>
-      <div>
+      <div className="todo-task__data-contianer" >
         <p className="todo-task__description">{task.description}</p>
-        <p className="todo-task__create-at">
+        <p className="todo-task__created-at">
           {format(task.createdAt, "dd/MMMM/yyyy", { locale: es })}
         </p>
       </div>
