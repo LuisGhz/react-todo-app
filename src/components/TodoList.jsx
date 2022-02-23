21da6b0dec8dea219a6b4ac08cd3e8bde34022cb
Luis Antonio Gutiérrez Hernández <luisghtz@outlook.com>
2022-02-23 16:26:24 -0600

    Change api endpoint. Integrate TodoTask

--- src/components/TodoList.jsx
+++ src/components/TodoList.jsx
@@ -1,12 +1,13 @@
 import React, { useContext, useEffect } from 'react';
 import axios from 'axios';
 import { TodoContext } from 'TodoContext';
+import { TodoTask } from './TodoTask';
 
 export const TodoList = () => {
   const { tasks, setTasks } = useContext(TodoContext);
   useEffect(() => {
     (async () => {
-      const r = await axios.get("http://localhost:3000/db");
+      const r = await axios.get("http://localhost:3000/todos");
       setTasks(r.data);
     })();
   // eslint-disable-next-line react-hooks/exhaustive-deps
@@ -16,8 +17,8 @@ export const TodoList = () => {
       {tasks.length === 0 && <p>You still have not a todo registered.</p> }
       {tasks.length > 0 && 
         <ul>
-          { tasks.map(el => (
-            <li key={el.id}>{el.description}</li>
+          { tasks.map(task => (
+            <li key={task.id}><TodoTask key={task.id} task={task} /></li>
           )) }
         </ul> 
       }
