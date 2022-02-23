f86664ab8041e7eefc897df4d61e26ef13a673b0
Luis Antonio Gutiérrez Hernández <luisghtz@outlook.com>
2022-02-22 19:55:18 -0600

    Mock axios

--- src/__mocks__/axios.js
+++ src/__mocks__/axios.js
@@ -1,28 +1,5 @@
-"use strict";
-module.exports = {
-  post: (todo) => {
-    return Promise.resolve({ ...todo });
-  },
-  get: () => {
-    return Promise.resolve([
-      {
-        id: 1,
-        description: "Task 1",
-        createdAt: new Date(),
-        isCompleted: false,
-      },
-      {
-        id: 2,
-        description: "Task 2",
-        createdAt: new Date(),
-        isCompleted: false,
-      },
-      {
-        id: 3,
-        description: "Task 3",
-        createdAt: new Date(),
-        isCompleted: false,
-      },
-    ]);
-  },
+/* eslint-disable import/no-anonymous-default-export */
+export default {
+  get: jest.fn().mockResolvedValue(),
+  post: jest.fn().mockResolvedValue()
 };
