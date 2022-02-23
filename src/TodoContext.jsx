c42cd2be9fc823ae6c6be0db805eabe95f554790
Luis Antonio Gutiérrez Hernández <luisghtz@outlook.com>
2022-02-22 20:39:54 -0600

    Handle empty array state

--- src/TodoContext.jsx
+++ src/TodoContext.jsx
@@ -1,24 +1,30 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect } from "react";
 
-export const TodoContext = React.createContext()
+export const TodoContext = React.createContext();
 
 export const TodoProvider = ({ children }) => {
   const [tasks, setTasks] = useState([]);
   const [totalTasksCompleted, setTotalTasksCompleted] = useState(0);
 
   useEffect(() => {
-    const totalT = tasks.filter(t => t.isCompleted).length;
-    setTotalTasksCompleted(totalT);
+    if (tasks.length > 0) {
+      const totalT = [...tasks];
+      setTotalTasksCompleted(totalT.filter((t) => t.isCompleted).length);
+      return;
+    }
+    setTotalTasksCompleted(0);
   }, [tasks]);
 
   return (
-    <TodoContext.Provider value={{
-      tasks,
-      setTasks,
-      totalTasksCompleted,
-      setTotalTasksCompleted
-    }}>
-      { children }
+    <TodoContext.Provider
+      value={{
+        tasks,
+        setTasks,
+        totalTasksCompleted,
+        setTotalTasksCompleted,
+      }}
+    >
+      {children}
     </TodoContext.Provider>
   );
-}
+};
