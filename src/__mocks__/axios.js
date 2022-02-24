b810652e1738bf22dec0f5244f18a5477f7319ea
Luis Antonio Gutiérrez Hernández <luisghtz@outlook.com>
2022-02-23 18:18:42 -0600

    Add put and delete mocks

--- src/__mocks__/axios.js
+++ src/__mocks__/axios.js
@@ -1,5 +1,7 @@
 /* eslint-disable import/no-anonymous-default-export */
 export default {
   get: jest.fn().mockResolvedValue(),
-  post: jest.fn().mockResolvedValue()
+  post: jest.fn().mockResolvedValue(),
+  put: jest.fn(),
+  delete: jest.fn()
 };
